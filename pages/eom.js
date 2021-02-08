22d1fa29f391190822bf5903fdccfbcda0cc491b
Ulugbek <u.isroilov@student.inha.uz>
2021-02-08 09:14:36 +0500

    init

---
+++ pages/eom.js
@@ -0,0 +1,36 @@
+import { Toolbar } from "../components/toolbar"
+import styles from "../styles/EOM.module.css"
+
+export const EOM = ({employee}) => {
+    console.log(employee)
+    return (
+        <div className="page-container">
+            <Toolbar /> 
+            <div className={styles.main}>
+                <h1>Employee of the month</h1>
+                <div className={styles.employeeOfTheMonth}>
+                    <h3>{employee.name}</h3>
+                    <h6>{employee.position}</h6>
+                    <img src={employee.image} />
+                    <p>{employee.description}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export const getServerSideProps = async pageContent  => {
+    const apiResponse = await fetch(
+        'https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth',
+    );
+
+    const employee = await apiResponse.json();
+
+    return {
+        props: {
+            employee
+        }
+    }
+}
+
+export default EOM;
